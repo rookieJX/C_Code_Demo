54f85fb2dfc0949dc4a0ee1ca21af30694c1edd3
rookieJX <wangjiaxiangyx@163.com>
2017-10-13 00:20:53 +0800

    万年历
    万年历完善，存入数组

--- 016.编制万年历/calendar.c
+++ 016.编制万年历/calendar.c
@@ -70,14 +70,11 @@ int month_current_day(int month) {
 /*根据年份，月份，求当月天数*/
 int year_month_day(int year,int month) {
 	// 首先定义一个已知年份第一月的第一天是周几，然后开始计算（已知1900年1月1日，是周一）
-	int leap_year_count = 0;// 普通一年是365天，但是闰年有366天（2月份为29天）
-	int month_totaly_day = 0;  // 总天数
-	for (int i = 1900; i <= year; i++)
+	int leap_year_count = 0;	// 普通一年是365天，但是闰年有366天（2月份为29天）
+	int month_totaly_day = 0;  	// 总天数
+	if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
 	{
-		if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
-		{
-			leap_year_count = 1;
-		}
+		leap_year_count = 1;
 	}
 	month_totaly_day = month_current_day(month); // 计算当月之前天数
 	if (month == January)  // 如果是从2开月开始
@@ -87,7 +84,8 @@ int year_month_day(int year,int month) {
 
 	return month_totaly_day;
 }
-/*返回某一年第一天为周几(周日为7)*/
+
+/*返回某一年第一天为周几(周日为6,周一为0)*/
 int year_week_day(int year) {
 	// 首先定义一个已知年份第一月的第一天是周几，然后开始计算（已知1900年1月1日，是周一）
 	int leap_year_count = 0;// 普通一年是365天，但是闰年有366天（2月份为29天）
@@ -96,15 +94,14 @@ int year_week_day(int year) {
 	{	
 		for (int i = 1900; i < year; i++)
 		{
-			if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+			if (((i % 4 == 0) && (i % 100 != 0)) || (i % 400 == 0))
 			{
 				leap_year_count ++;
 			}
 		}
 		year_totaly_day = (year - 1900) * 365 + leap_year_count;
 	}
-
-	return year_totaly_day % 7 + 1;
+	return year_totaly_day % 7;
 }
 
 /*根据年份，以及月份，计算当前月份第一天是星期几*/
@@ -112,67 +109,66 @@ int year_month_week_day(int year,int month) {
 	// 首先定义一个已知年份第一月的第一天是周几，然后开始计算（已知1900年1月1日，是周一）
 	int leap_year_count = 0;// 普通一年是365天，但是闰年有366天（2月份为29天）
 	int month_totaly_day = 0;  // 总天数
-	for (int i = 1900; i <= year; i++)
+	if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
 	{
-		if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
-		{
-			leap_year_count = 1;
-		}
+		leap_year_count = 1;
 	}
 	month_totaly_day = month_day(month); // 计算当月之前天数
-	if (month > January)  // 如果是从2开月开始
+	if (month > February)  // 如果是从2开月开始
 	{
 		month_totaly_day += leap_year_count;
 	}
+	return (month_totaly_day % 7 + year_week_day(year))%7;
 
-	return month_totaly_day % 7 + year_week_day(year);
 }
 
 
 /*根据年份，打印月份，以及星期*/
 void year_month(int year) {
+	int calendar_day[12][6][7] = {0}; // 定义三位数组存放日期
 	printf("\n|=====================The Calendar of Year %d ========================|\n",year );
 	char weeks[][7] = {"MON","TUE","WED","THU","FRI","SAT","SUN"};
 	for (int i = 1; i <= 12; i++)
 	{
-		int week_day = 1; // 默认是周一
+		int week_day = 0,week_count = 4; // 默认是周一,默认一个月有4周
 		int day = 30;  // 默认为30天
-		int calendar_day[12][6][7]; // 定义三位数组存放日期
 		week_day = year_month_week_day(year,i); // 计算这个月的第一天是周几
-		day  = year_month_day(year,i) + week_day - 1; // 这个月总共有多少天（从礼拜一开始算起）
+		day  = year_month_day(year,i) + week_day ; // 这个月总共有多少天（从礼拜一开始算起）
+		int weeks_month = 0;		// 记录当前日期，在本月是第几个星期
+		int week_day_month = 0;		// 记录当前日期，是礼拜几
+		for (int k = 1; k <= day; k++) // 
+		{
+			weeks_month	= (k-1)/7;
+			week_day_month	= (k-1)%7;
+			if (k <= week_day)
+			{
+				calendar_day[i-1][weeks_month][week_day_month] = 0;
+			}
+			else
+			{
+				calendar_day[i-1][weeks_month][week_day_month] = k-week_day;
+			}
+		}
+	}
 
+	for (int i = 0; i < 12; i++)
+	{
+		printf("%-2d  ",i );
+		for (int j = 0; j < 7; j++)
+		{
+			printf("%s  ",weeks[j] );
+		}
+		printf("\n    ");
 		for (int j = 0; j < 6; j++)
 		{
 			for (int k = 0; k < 7; k++)
 			{
-				
+				printf("%-2d   ", calendar_day[i][j][k]);
 			}
+			printf("\n    ");
 		}
+		printf("\b\b\b\b==================================================\n");
 
-		// printf("%d年：第%d月第一天为礼拜%d，这个月共有%d天\n",year,i,week_day,day );
-		// 开始打印
-		printf("%-2d  ", i);
-		for (int k = 0; k < 7; k++)
-		{
-			printf("%s ", weeks[k]);
-		}
-		printf("\n    ");
-		for (int j = 1; j <= day; j++) {
-			if (j < week_day)
-			{
-				printf("    ");
-			}
-			else
-			{
-				printf("%-2d  ",j-week_day+1);
-			}
-			if (j % 7 == 0)
-			{
-				printf("\n    ");
-			}
-		}
-
-		printf("\n==================================================\n");
 	}
 }
 
