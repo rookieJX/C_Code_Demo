1955bb1a3d475918dbc847d3ee078dfcc9c6ac1f
zzhrianbow <rookieJX@users.noreply.github.com>
2017-11-14 13:39:07 +0800

    题目：打印楼梯，同时在楼梯上方打印两个笑脸。
    程序分析：用 ASCII 1 来输出笑脸；用i控制行，j来控制列，j根据i的变化来控制输出黑方格的个数。

---
+++ C语言100例/C010/C010.c
@@ -0,0 +1,18 @@
+/*
+题目：打印楼梯，同时在楼梯上方打印两个笑脸。
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int i,j;
+	printf("\1\1\n"); // 输出两个笑脸
+	 for(i=1;i<11;i++)
+    {
+        for(j=1;j<=i;j++)
+            printf("%c%c",219,219);
+        printf("\n");
+    }
+	return 0;
+}
