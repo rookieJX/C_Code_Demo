795422ba53f3d8002002c66ea55b37d37ec237d6
rookieJX <wangjiaxiangyx@163.com>
2017-11-14 22:11:17 +0800

    题目：将一个正整数分解质因数。例如：输入90,打印出90=2*3*3*5。
    程序分析：对n进行分解质因数，应先找到一个最小的质数k，然后按下述步骤完成：
    (1)如果这个质数恰等于（小于的时候，继续执行循环）n，则说明分解质因数的过程已经结束，另外 打印出即可。
    (2)但n能被k整除，则应打印出k的值，并用n除以k的商,作为新的正整数n.重复执行第二步。
    (3)如果n不能被k整除，则用k+1作为k的值,重复执行第一步。

--- C语言100例/C014/C014.c
+++ C语言100例/C014/C014.c
@@ -2,10 +2,30 @@
 题目：将一个正整数分解质因数。例如：输入90,打印出90=2*3*3*5。
 */
 
-#include <sdtio.h>
+#include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
-	
+	int i , j = 2;
+	printf("Input a number : ");
+	scanf("%d",&i);
+	while(i < 2) {
+		printf("The number must large than 2.Please try again : ");
+		scanf("%d",&i);
+	}
+	printf("%d = ",i );
+	while(j <= i) {
+		if (i % j == 0)
+		{
+			printf("%d",j );
+			i /= j;
+			j = 1;
+		}
+		if (j == 1)
+		{
+			printf("*");
+		}
+		j ++;
+	}
 	return 0;
 }
