ebd00f21741bbe69065d4cf729daceefbc7472f5
zzhrianbow <rookieJX@users.noreply.github.com>
2017-11-15 18:11:56 +0800

    题目：一个数如果恰好等于它的因子之和，这个数就称为"完数"。例如6=1＋2＋3.编程找出1000以内的所有完数

--- C语言100例/C019/C019.c
+++ C语言100例/C019/C019.c
@@ -7,28 +7,11 @@
 int main(int argc, char const *argv[])
 {
 	int sum=0;
-	for (int i = 2; i <= 10; i++)
-	{
-		int j=2;
-		while(j <= i) {
-			if (i % j == 0)
-		    {
-			    i /= j;
-			    sum += j;
-			    printf("%d",j );
-			    j = 1;
-		    }
-		    if(j == 1)
-		    	printf("+");
-		    j++;
-		}
-		if (j == 2)
-		{
-			printf("=%d\n", sum);
-		}
+	int i = 6;
+	while() {
 		
-	
-    }
+	}
+
 
 	return 0;
 }
